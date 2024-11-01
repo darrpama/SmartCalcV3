bd5616271861d12c6e1b99fda27179bf46e84d4b
darrpama <darrpama@darrpamas-MacBook-Air.local>
2024-11-01 08:57:36 +0300

    just add headers

---
+++ src/SmartCalcV3.0/SmartCalcV3/CoreLibs/DllInterface.h
@@ -0,0 +1,25 @@
+#ifndef DLLINTERFACE_H
+#define DLLINTERFACE_H
+
+#include "Model.h"
+
+s21::Model _model = s21::Model();
+std::vector<double> x;
+std::vector<double> y;
+
+extern "C" __attribute__ ((visibility ("default")))
+const char* CalculateWrapper(const char* expression);
+
+extern "C" __attribute__ ((visibility ("default")))
+const char* GetGraphWrapper(const char* expression, double leftBound, double rightBound);
+
+extern "C" __attribute__ ((visibility ("default")))
+double* GetXArrayWrapper();
+
+extern "C" __attribute__ ((visibility ("default")))
+double* GetYArrayWrapper();
+
+extern "C" __attribute__ ((visibility ("default")))
+void FreeArrayMemoryWrapper(double* ptr);
+
+#endif  // DLLINTERFACE_H
